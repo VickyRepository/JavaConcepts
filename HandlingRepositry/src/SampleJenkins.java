080c9eaad4824eefca831e3a0d83d4272703a14e
VickyRepository <vignesh2491995@gmail.com>
2019-06-23 10:03:50 +0530

    Test

--- HandlingRepositry/src/SampleJenkins.java
+++ HandlingRepositry/src/SampleJenkins.java
@@ -5,7 +5,7 @@ public class SampleJenkins {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		System.out.println("It's jenkins Configuration Git Test config");
+		System.out.println("It's jenkins Configuration");
 
 	}
 
